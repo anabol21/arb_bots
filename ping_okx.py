8dcc84a25653aef26155d711fc5d73e42889b71f
anabol21 <your-email@example.com>
2025-09-26 15:07:06 +0300

    okx ping check

---
+++ ping_okx.py
@@ -0,0 +1,24 @@
+import websocket
+import json
+import time
+from datetime import datetime
+
+def on_message(ws, message):
+    data = json.loads(message)
+    if "data" in data and "ts" in data["data"][0]:
+        ts_exchange = int(data["data"][0]['ts'])
+        ts_local = int(time.time() * 1000)
+
+        latency = ts_local - ts_exchange
+        print(f"Market data latency: {latency} ms")
+
+def on_open(ws):
+    sub_msg = {
+        "op": "subscribe",
+        "args":[{"channel": "tickers", "instId": "BTC-USDT-SWAP"}]
+    }
+    ws.send(json.dumps(sub_msg))
+
+url = "wss://ws.okx.com:8443/ws/v5/public"
+ws = websocket.WebSocketApp(url, on_message=on_message, on_open=on_open)
+ws.run_forever()
